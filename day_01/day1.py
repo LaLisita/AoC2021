3d4b66693ed321a42f82ea0b7f36ec28d3b3366b
Lisa Brueckbauer <lisa.brueckbauer.ext@siemens-healthineers.com>
2021-12-01 16:12:59 +0100

    day_01 part 2

--- day_01/day1.py
+++ day_01/day1.py
@@ -2,11 +2,11 @@ filename = "day_01/input.txt"
 
 with open(filename) as file:
     lines = file.readlines()
-    prev = int(lines[0])
-    incCount = 0
+prev = int(lines[0]) + int(lines[1]) + int(lines[2])
+incCount = 0
 
-for l in lines[1::]:
-    number = int(l)
+for i in range(len(lines)-2):
+    number = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
     if number > prev:
         incCount+=1
     prev = number
